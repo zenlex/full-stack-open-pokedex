632ca418a168371cf653df84388039fc987ebb35
Erich Keil <39918654+zenlex@users.noreply.github.com>
2022-05-22 19:44:01 -0500

    Move master to main
    * add hello workflow
    
    * add hello workflow
    
    * fix syntax
    
    * fix syntax
    
    * fix syntax
    
    * add date and dir
    
    * split date and dir jobs
    
    * split date and dir jobs
    
    * change dir-contents
    
    * Start Depoloyment Pipeline
    
    - add ubuntu setup
    - add src code checkout
    - add node setup
    - add dependency install
    
    * add linting to pipeline
    
    * fix linting issues
    
    * fix linting issues
    
    * add build and test to pipeline
    
    * fix next link bug
    
    * add e2e test to pipeline
    
    * add e2e test to pipeline
    
    * switch cypress browser to chrome
    
    * change cypress to run
    
    * add second e2e test
    
    * configure heroku deploy
    
    * yaml cleanup
    
    * yaml cleanup
    
    * add procfile
    
    * add healthcheck
    
    * add rollback
    
    * fix healthcheck url for heroku
    
    * test deploy on main merge only
    
    * typo fix
    
    * typo fix
    
    * add release tagging
    
    * move if statement to skip check setup on PR
    
    * add skip ability
    
    * fix syntax
    
    * fix syntax
    
    * fix syntax
    
    * add skip condition
    
    * test tag
    
    * test tag
    
    * test tag
    
    * test tag
    
    * test tag
    
    * test tag
    
    * test #skip
    
    * test #skip
    
    * tag after skipped commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -16,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
@@ -43,7 +45,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    // "no-console": "error",
     "react/prop-types": 0
   }
 }
